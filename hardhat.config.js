54b021506e12cdc6ac00ae89d6813a2db5c159ff
Horacio Lopez <hlopez3rd@gmail.com>
2021-08-17 11:00:44 -0500

    Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
+ * hardhat.config.js
  */
+
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+
 module.exports = {
-  solidity: "0.7.3",
-};
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
